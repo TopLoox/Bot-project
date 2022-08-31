7fd87df0f651fce602583f9fde85f0a9ac55ff0d
_Meegont_ <Meegont.m@gmail.com>
2022-08-31 21:36:08 +0300

    Create quasar project

---
+++ src/pages/MainApp.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-intersection transition="scale">
+      <q-card class="flex flex-center column shadow-0 q-pa-sm">
+        <img
+          class="q-mt-md q-mb-sm"
+          style="width: 7.5em; height: 7.5em;"
+          src="~assets/logo.png"
+        >
+
+        <span class="q-mx-md">Bot project</span>
+      </q-card>
+    </q-intersection>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainApp'
+})
+</script>
